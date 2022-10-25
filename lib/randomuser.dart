566f9a07484cf049f9a1706c7098838c4a3e8872
ZARIFMAC <uzbzarif@gmail.com>
2022-10-25 16:10:15 +0500

    User class

---
+++ lib/randomuser.dart
@@ -0,0 +1,12 @@
+class User {
+  String? gender;
+  String? name;
+  String? email;
+  String? phone;
+  String? picture;
+  String? location;
+  String? age;
+  String? date;
+  String? city;
+  String? country;
+}
